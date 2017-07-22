0470d1e26af34f85ea42d5a2619f11a3e925ec42
libraminh <libraminh@gmail.com>
2017-07-22 10:14:07 +0700

    ...

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -7,11 +7,65 @@ $(document).ready(function(){
 });
 
 function clickTab() {
-  var linkTab = $('section.service-func li a');
+  var linkTab = $('section.service-func li a'),
+      linkTabCarousel = $('.direction .url'),
+      linkTabService = $('section.service-func-service .service-func-tab li a');
 
+  // Service Func
   $(linkTab).on('click', function(){
     $(this).addClass('active');
     $(this).parent().siblings().find('a').removeClass('active');
+  })
+
+  // Price Carousel
+  $(linkTabCarousel).on('click', function(){
+    $(this).addClass('open');
+    $(this).parent().siblings().find('.url').removeClass('open');
+  })
+
+  // Service Func Service
+  function stuff() {
+    $(linkTabService).focusin(function(){
+      $(this).addClass('index');
+    })
+    $(linkTabService).focusout(function(){
+      $(this).removeClass('index');
+    })
+  }
+
+  function stuff2() {
+    $(linkTabService).focusin(function(){
+      $(this).addClass('index-open');
+    })
+    $(linkTabService).focusout(function(){
+      $(this).removeClass('index-open');
+    })
+  }
+
+  function stuff3() {
+    $(linkTabService).removeClass('index');
+    $(linkTabService).focusin(function(){
+      $(this).removeClass('index');
+    })
+    $(linkTabService).focusout(function(){
+      $(this).removeClass('index');
+    })
+    $(linkTabService).removeClass('index-open');
+  }
+
+  if($(window).width() > 990) {
+    stuff();
+  } else {
+    stuff2();
+  }
+
+  $(window).resize(function(){
+
+    if($(window).width() > 990) {
+      stuff();
+    } else {
+      stuff3();
+    }
   })
 
 }
@@ -32,17 +86,23 @@ function scrollAnimate() {
   sr.reveal('.owl-carousel', { duration: 1500, distance: '100px'});
 
   // Index Page
+  sr.reveal('.index-stuff .wrap-customer', { duration: 1500, distance: '50px' });
   sr.reveal('.wrap-rent', { duration: 1500, distance: '50px' });
   sr.reveal('section.index-contact .col-sm-6:first-child', { duration: 1500, distance: '200px', origin: 'left' });
+  sr.reveal('.service-stuff-1', { duration: 1500, distance: '200px', origin: 'right' });
+  sr.reveal('.contact-img', { duration: 1500, distance: '200px', origin: 'right' });
   sr.reveal('.left-mes', { duration: 1500, distance: '200px', origin: 'left' });
   sr.reveal('.wrap-video', { duration: 1500, distance: '100px'});
 
   // Service Page
   sr.reveal('section.service-sale .col-sm-6:first-child', { duration: 1500, distance: '200px', origin: 'left' });
   sr.reveal('.left-service-func', { duration: 1500, distance: '100px', origin: 'left'});
+  sr.reveal('.service-exam .wrap-customer', { duration: 1500, distance: '50px'});
+  sr.reveal('.service-func-service .wrap-customer', { duration: 1500, distance: '50px'});
 
   // Contact Page
   sr.reveal('section.contact-us .col-sm-6:first-child', { duration: 1500, distance: '100px', origin: 'left'});
+  sr.reveal('section.contact-us .contact-showcase', { duration: 1500, distance: '200px', origin: 'right' });
 }
 
 function customcarousel() {
@@ -52,13 +112,12 @@ function customcarousel() {
         loop:true,
         center:true,
         margin: 0,
-        navText: ['<img class="left-arrow" src="assets/img/svg/right-chevron.svg" >','<img class="right-arrow" src="assets/img/svg/right-chevron.svg" >'],
+        navText: ['<span class="navi-btn pre-btn"> <i class="fa fa-angle-left" aria-hidden="true"></i> </span>','<span class="navi-btn pre-btn"> <i class="fa fa-angle-right" aria-hidden="true"></i> </span>'],
         navElement: 'span',
         nav: true,
         URLhashListener:true,
         autoplayHoverPause:true,
         startPosition: 'URLHash',
-        animateIn: 'jackInTheBox',
     });
     // Index Carousel, Service Carousel
     $('.carousel-2, .carousel-4').owlCarousel({
@@ -66,7 +125,8 @@ function customcarousel() {
       loop: true,
       margin: 0,
       nav: true,
-      navText: ['<img class="index-arrow index-left-arrow" src="assets/img/svg/arrow.svg" >','<img class="index-arrow index-right-arrow" src="assets/img/svg/arrow.svg" >'],
+      navElement: 'span',
+      navText: ['<div class="box-icon box-left"><i class="fa fa-angle-left navPage left-nav" aria-hidden="true"></i></div>','<div class="box-icon box-right"><i class="fa fa-angle-right navPage right-nav" aria-hidden="true"></i></div>'],
       responsive:{
         0:{
             items:1,
@@ -86,7 +146,7 @@ function customcarousel() {
       items: 3,
       loop: true,
       nav: true,
-      navText: ['<img class="index-arrow index-left-arrow" src="assets/img/svg/arrow.svg" >','<img class="index-arrow index-right-arrow" src="assets/img/svg/arrow.svg" >'],
+      navText: ['<div class="box-icon box-left"><i class="fa fa-angle-left navPage left-nav" aria-hidden="true"></i></div>','<div class="box-icon box-right"><i class="fa fa-angle-right navPage right-nav" aria-hidden="true"></i></div>'],
       navElement: 'span',
       margin: 20,
       responsiveClass:true,
